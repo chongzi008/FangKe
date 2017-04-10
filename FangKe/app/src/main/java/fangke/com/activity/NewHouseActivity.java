8e26ae423d6c3daad6e969363a1f4048a16ee099
chongzi008 <977964494@qq.com>
2017-04-10 20:09:51 +0800

    完成新房的页面下点击附近弹出的页面

--- FangKe/app/src/main/java/fangke/com/activity/NewHouseActivity.java
+++ FangKe/app/src/main/java/fangke/com/activity/NewHouseActivity.java
@@ -1,18 +1,25 @@
 package fangke.com.activity;
 
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.PopupWindow;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * 新房页面
@@ -28,6 +35,8 @@ public class NewHouseActivity extends AppCompatActivity {
     public final static int Type_3 = 2;
     private ListView lv;
     private ArrayList list;
+    private TextView tv_near;
+    private ImageView img_near;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,9 +45,113 @@ public class NewHouseActivity extends AppCompatActivity {
         setContentView(R.layout.activity_new_house);
         initData();
         initViews();
+        initListener();
 
     }
 
+    private void initListener() {
+        //初始化新房附近的监听。
+        tv_near.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //点击附近的时候我们就弹出下拉窗口
+                showNearWindow();
+
+            }
+        });
+        img_near.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //点击附近的时候我们就弹出下拉窗口
+                showNearWindow();
+            }
+        });
+    }
+
+    private void showNearWindow() {
+        //弹出附近窗口
+        View nearview = View.inflate(NewHouseActivity.this, R.layout.newhouse_popupwindow_near, null);
+        final ListView near_lv_left = (ListView) nearview.findViewById(R.id.newhouse_popupview_near_lv_left);
+        final ListView near_lv_right = (ListView) nearview.findViewById(R.id.newhouse_popupview_near_lv_right);
+
+        SimpleAdapter nearAdapter=new SimpleAdapter(NewHouseActivity.this,getNearWindowData(),R.layout.popupwindow_near_left_item,
+                new String[]{"title"},new int[]{R.id.popupwindow_near_left});
+        near_lv_left.setAdapter(nearAdapter);
+        near_lv_left.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if(position==0){
+                      near_lv_right.setVisibility(View.INVISIBLE);
+                }else if(position==1){
+                    if(near_lv_right.getVisibility()==View.VISIBLE){
+
+                    }else {
+
+                        near_lv_right.setVisibility(View.VISIBLE);
+                        ArrayList<HashMap<String, Object>> arrayList = new ArrayList<HashMap<String, Object>>();
+                        HashMap<String, Object> tempHashMap1 = new HashMap<String, Object>();
+                        tempHashMap1.put("title", "不限");
+                        HashMap<String, Object> tempHashMap2 = new HashMap<String, Object>();
+                        tempHashMap2.put("title", "香洲");
+                        HashMap<String, Object> tempHashMap3 = new HashMap<String, Object>();
+                        tempHashMap3.put("title", "金湾");
+                        HashMap<String, Object> tempHashMap4 = new HashMap<String, Object>();
+                        tempHashMap4.put("title", "斗门");
+                        HashMap<String, Object> tempHashMap5 = new HashMap<String, Object>();
+                        tempHashMap5.put("title", "中山市");
+                        HashMap<String, Object> tempHashMap6 = new HashMap<String, Object>();
+                        tempHashMap6.put("title", "横琴");
+                        HashMap<String, Object> tempHashMap7 = new HashMap<String, Object>();
+                        tempHashMap7.put("title", "高栏港经济区");
+                        HashMap<String, Object> tempHashMap8 = new HashMap<String, Object>();
+                        tempHashMap8.put("title", "其他");
+
+                        arrayList.add(tempHashMap1);
+                        arrayList.add(tempHashMap2);
+                        arrayList.add(tempHashMap3);
+                        arrayList.add(tempHashMap4);
+                        arrayList.add(tempHashMap5);
+                        arrayList.add(tempHashMap6);
+                        arrayList.add(tempHashMap7);
+                        arrayList.add(tempHashMap8);
+                        SimpleAdapter nearRightAdapter = new SimpleAdapter(NewHouseActivity.this, arrayList, R.layout.popupwindow_near_right_item,
+                                new String[]{"title"}, new int[]{R.id.popupwindow_near_right});
+                        near_lv_right.setAdapter(nearRightAdapter);
+
+                    }
+
+                }else if(position==2){
+                    near_lv_right.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
+
+        PopupWindow nearPopupWindow = new PopupWindow(nearview, ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT, true);
+        nearPopupWindow.setAnimationStyle(R.style.popwin_near_anim_style);
+        nearPopupWindow.setTouchable(true);
+        nearPopupWindow.showAtLocation(tv_near, Gravity.TOP, 0, 0);
+        nearPopupWindow.setBackgroundDrawable(new ColorDrawable(0x00000000));
+
+    }
+
+    private ArrayList<HashMap<String, Object>> getNearWindowData(){
+        ArrayList<HashMap<String, Object>> arrayList = new ArrayList<HashMap<String,Object>>();
+        HashMap<String, Object> tempHashMap1 = new HashMap<String, Object>();
+        tempHashMap1.put("title","附近");
+        HashMap<String, Object> tempHashMap2 = new HashMap<String, Object>();
+        tempHashMap2.put("title","区域");
+        HashMap<String, Object> tempHashMap3 = new HashMap<String, Object>();
+        tempHashMap3.put("title","地铁");
+
+        arrayList.add(tempHashMap1);
+        arrayList.add(tempHashMap2);
+        arrayList.add(tempHashMap3);
+
+
+        return arrayList;
+
+    }
 
     private void initData() {
 
@@ -123,10 +236,10 @@ public class NewHouseActivity extends AppCompatActivity {
     }
 
     private void initViews() {
-
+        tv_near = (TextView) findViewById(R.id.newhouse_tv_near);
+        img_near = (ImageView) findViewById(R.id.newhouse_img_near);
         lv = (ListView) findViewById(R.id.newhouse_lv);
         lv.setAdapter(new MyAdapter());
-
     }
 
     class MyAdapter extends BaseAdapter {
@@ -192,7 +305,7 @@ public class NewHouseActivity extends AppCompatActivity {
                         viewHolderNormal.title = (TextView) convertView.findViewById(R.id.newhouse_tv_normalview_title);
                         viewHolderNormal.area = (TextView) convertView.findViewById(R.id.newhouse_tv_normalview_area);
                         viewHolderNormal.discount = (TextView) convertView.findViewById(R.id.newhouse_tv_normalview_discount);
-                        viewHolderNormal.money= (TextView) convertView.findViewById(R.id.newhouse_tv_normalview_money);
+                        viewHolderNormal.money = (TextView) convertView.findViewById(R.id.newhouse_tv_normalview_money);
                         convertView.setTag(viewHolderNormal);
                         break;
                     case Type_3:
@@ -227,11 +340,11 @@ public class NewHouseActivity extends AppCompatActivity {
                     viewHolderHead.tv.setText("找到4个楼盘吧");
                     break;
                 case Type_2:
-                    ArrayList l = (ArrayList) list.get(position-1);
+                    ArrayList l = (ArrayList) list.get(position - 1);
                     viewHolderNormal.img.setBackgroundResource((Integer) l.get(0));
-                    viewHolderNormal.title.setText((String)l.get(1));
-                    viewHolderNormal.area.setText((String)l.get(2));
-                    viewHolderNormal.money.setText((String)l.get(3));
+                    viewHolderNormal.title.setText((String) l.get(1));
+                    viewHolderNormal.area.setText((String) l.get(2));
+                    viewHolderNormal.money.setText((String) l.get(3));
 
 
                     break;
@@ -240,7 +353,7 @@ public class NewHouseActivity extends AppCompatActivity {
                     viewHolderMiddle.btn.setOnClickListener(new View.OnClickListener() {
                         @Override
                         public void onClick(View v) {
-                            Toast.makeText(NewHouseActivity.this,"你真的要订阅吗?",Toast.LENGTH_SHORT).show();
+                            Toast.makeText(NewHouseActivity.this, "你真的要订阅吗?", Toast.LENGTH_SHORT).show();
                         }
                     });
                     break;
